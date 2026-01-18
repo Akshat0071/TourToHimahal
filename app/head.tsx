84e8b6a52ad121ddf4e3f71b1efb7097f36e6c33
Sidd1374 <akshatbansal04@gmail.com>
2026-01-19 00:40:04 +0530

    Added clarity

--- app/head.tsx
+++ app/head.tsx
@@ -25,6 +25,18 @@ export default function Head() {
 
       {/* LCP hint */}
       <link rel="preload" as="image" href={lcpHeroImage} fetchPriority="high" />
+
+      {/* Microsoft Clarity Analytics */}
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "v3ikvkpm3x");`,
+        }}
+      />
     </>
   )
 }
